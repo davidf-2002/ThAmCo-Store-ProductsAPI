43f78a6dfb319ded561fe40323708b37208fe417
davidf-2002 <140307515+davidf-2002@users.noreply.github.com>
2024-11-20 11:10:30 +0000

    Further corrected workflow 3

--- ProductsAPI/Data/ProductContext.cs
+++ ProductsAPI/Data/ProductContext.cs
@@ -10,7 +10,6 @@ public class ProductContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; } = null!;
-    // public DbSet<Suppliers> Suppliers { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -30,13 +29,6 @@ public class ProductContext : DbContext
             // entity.Property(e => e.Price)
             //       .HasColumnType("decimal(18,2)"); // Specify decimal with precision and scale
         });
-
-        // // Seed data for Suppliers
-        // modelBuilder.Entity<Suppliers>().HasData(
-        //     new Suppliers { Id = 1, Name = "Global Supplies", ContactInformation = "Contact Us: 123-456-7890" },
-        //     new Suppliers { Id = 2, Name = "Local Suppliers", ContactInformation = "Contact Us: 987-654-3210" },
-        //     new Suppliers { Id = 3, Name = "World Suppliers", ContactInformation = "Contact Uk: 01642-629-429"}
-        // );
 
         // Seed data for Products
         modelBuilder.Entity<Product>().HasData(
